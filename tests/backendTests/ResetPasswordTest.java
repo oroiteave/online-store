e3ed6e2be8fd48de6d8fe50702a697a5d5188c07
oroiteave <oroiteave@gmail.com>
2024-03-25 19:15:07 -0300

    intellij IDEA ide files implement

--- tests/backendTests/ResetPasswordTest.java
+++ tests/backendTests/ResetPasswordTest.java
@@ -1,9 +1,9 @@
 package backendTests;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.verify;
-
+import online_store.entities.User;
+import online_store.entities.impl.DefaultUser;
+import online_store.services.impl.DefaultResetPasswordService;
+import online_store.util.mail.MailSender;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -12,30 +12,29 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import online_store.services.impl.DefaultResetPasswordService;
-import online_store.util.mail.MailSender;
-import online_store.entities.User;
-import online_store.entities.impl.DefaultUser;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class ResetPasswordTest {
-	@InjectMocks
-	private DefaultResetPasswordService resetPasswordService = new DefaultResetPasswordService();
-	
 	@Mock
 	private MailSender mailSenderMock;
 
 	@Captor
 	private ArgumentCaptor<String> captor;
+	@InjectMocks
+	private DefaultResetPasswordService resetPasswordService = new DefaultResetPasswordService();
+
 
 	@Test
 	void shouldSendToUserEmail() {
 		User user = new DefaultUser();
 		String userEmail = "testemail@email.com";
 		user.setEmail(userEmail);
-		
+
 		resetPasswordService.resetPasswordForUser(user);
-		
+
 		verify(mailSenderMock).sendEmail(captor.capture(), anyString());
 		assertEquals(captor.getValue(), userEmail);
 	}
